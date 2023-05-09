20fc75e0d9eb3b5d2c23a9b2b603102d11635b91
Anil Patil <anilp1020@gmail.com>
2023-05-09 15:29:17 +0530

    Configuration File

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+CSV_FILE_Path='C:\Anil Data\Data\Python Test Jupyter\Project\ML_Classification Project_churn prediction\Project_app\Bank Customer Churn Prediction.csv'
+PORT_NUMBER = 5005
+MODEL_FILE_PATH=os.path.join('Project_app','ML-Churn_Prediction_Project.pkl')
+JSON_FILE_PATH=os.path.join('Project_app','project_data.json')
